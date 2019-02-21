ff70c49944a4fb0041f4c084026caaf85a7e8a73
Matthew James Briggs <matthew.james.briggs@gmail.com>
2019-02-21 13:43:56 -0800

    better mem store design

--- lib/store/mem_store.go
+++ lib/store/mem_store.go
@@ -5,89 +5,38 @@ package store
 import (
 	"github.com/webern/flog"
 	"github.com/webern/tftp/lib/tfcore"
+	"sync"
 )
 
 var _ Store = (*memStore)(nil)
 
-// memStore implements the Store interface for storing and retrieving files.
+// memStore implements the Store interface for storing and retrieving files in a memory cache.
 type memStore struct {
-	ch    chan Request
+	mx    sync.RWMutex // protects the files map
 	files map[string][]byte
 }
 
-// NewMemStore creates an in-memory file store which listens for requests on a goroutine. The goroutine is created by
-// the NewMemStore function. To destoy the goroutine call memStore.Terminate()
+// NewMemStore creates a new Store for storing and retrieving files to/from a memory cache.
 func NewMemStore() Store {
-	m := memStore{
-		ch:    make(chan Request),
-		files: make(map[string][]byte, 10),
-	}
-
-	go m.listen()
-	return &m
-}
-
-func (m *memStore) RequestChan() chan<- Request {
-	return m.ch
-}
-
-func (m *memStore) Terminate() {
-	resp := make(chan Response)
-	termReq := Request{Type: ReqTerm, File: nil, ResponseChan: resp}
-	go func() { m.ch <- termReq }()
-	//<-resp
-	close(m.ch)
-	return
-}
-
-func (m *memStore) listen() {
-	select {
-	case req := <-m.ch:
-		{
-			if req.Type == ReqPut {
-				err := m.put(req.File)
-				if req.ResponseChan != nil {
-					go func() { req.ResponseChan <- Response{err, nil} }()
-				}
-			} else if req.Type == ReqGet {
-				file, err := m.get(req.File)
-				if req.ResponseChan != nil {
-					go func() { req.ResponseChan <- Response{err, &file} }()
-				}
-			} else if req.Type == ReqTerm {
-				m.files = nil
-				if req.ResponseChan != nil {
-					go func() { req.ResponseChan <- Response{nil, nil} }()
-				}
-				return
-			}
-		}
+	return &memStore{
+		mx:    sync.RWMutex{},
+		files: make(map[string][]byte),
 	}
 }
 
-func (m *memStore) put(file *tfcore.File) error {
-	if file == nil {
-		return flog.Raise("file is nil")
+func (m *memStore) Get(name string) (tfcore.File, error) {
+	m.mx.RLock()
+	defer m.mx.RUnlock()
+	if b, ok := m.files[name]; ok {
+		f := tfcore.File{}
+		f.Name = name
+		f.Data = make([]byte, len(b), len(b))
+		return f, nil
 	}
 
-	b := make([]byte, len(file.Data), len(file.Data))
-	copy(b, file.Data)
-	m.files[file.Name] = b
+	return tfcore.File{}, flog.Raisef("the file '%s' was not found", name)
+}
+
+func (m *memStore) Put(f tfcore.File) error {
 	return nil
-}
-
-func (m *memStore) get(file *tfcore.File) (tfcore.File, error) {
-	if file == nil {
-		return tfcore.File{}, flog.Raise("file is nil, you need to pass the filename using file.Name")
-	}
-
-	b, ok := m.files[file.Name]
-	file.Data = make([]byte, len(b), len(b))
-	copy(file.Data, b)
-
-	if !ok {
-		return tfcore.File{}, flog.Raisef("the file '%s' was not found", file.Name)
-	}
-
-	return *file, nil
 }
--- lib/store/store.go
+++ lib/store/store.go
@@ -5,63 +5,11 @@ package store
 import "github.com/webern/tftp/lib/tfcore"
 
 type Store interface {
+	// Put stores a file in the Store (overwriting if a file by the same name exists). Set is safe for concurrent
+	// goroutine access. The file is deep copied before storing.
+	Put(f tfcore.File) error
 
-	// Chan returns a channel that can be used to store and retrieve files to and from the Store
-	RequestChan() chan<- Request
-
-	// Terminate destroys the memory store, closes its request channel, and leaves it in an unusable state.
-	Terminate()
+	// Get returns a file from the Store or an error if it is not found. Get is safe for concurrent goroutine access.
+	// The returned file is a deep copy of the stored file, you may mutate it without affecting the Store.
+	Get(name string) (tfcore.File, error)
 }
-
-type Response struct {
-	Err  error
-	File *tfcore.File
-}
-
-type RequestType int
-
-const (
-	ReqPut  RequestType = iota // Request to Put a file into the Store
-	ReqGet                     // Request to Get a file from the Store
-	ReqTerm                    // Request the termination of the Store and it's goroutine
-)
-
-type Request struct {
-	Type         RequestType
-	File         *tfcore.File
-	ResponseChan chan<- Response
-}
-
-//// PutResponse is used to signal the completion of an atomic operation in which a File is stored in the Store
-//type PutResponse struct {
-//	// Err represents an error condition in which the file was not stored.
-//	Err error
-//}
-//
-//// PutRequest represents a request to store a file in the Store. A response will be sent on ResponseChan if not nil.
-//type PutRequest struct {
-//	// File represents the File that we are requesting be stored in the Store.
-//	tfcore.File
-//
-//	// ResponseChan will be used to signal the completion of the put operation. It is optional and can be left nil.
-//	// The caller supplies ResponseChan if the caller wishes to wait for successful completion of the operation.
-//	ResponseChan chan PutResponse
-//}
-//
-//// GetRequest represents a request sent to the Store to retrieve a file by name.
-//type GetRequest struct {
-//	// Name represents the file name of the file that we wish to retrieve from the Store.
-//	Name string
-//
-//	// ResponseChan must be supplied by the caller, this is how the data will be returned to the caller from the store.
-//	ResponseChan chan GetResponse
-//}
-//
-//// GetResponse represents a response sent back from the Store for a given request.
-//type GetResponse struct {
-//	// Err represents any error that occurred while trying to retrieve the file. For example, file not found.
-//	Err error
-//
-//	// File represents a deep copy of the retrieved file, unless Err is not nil.
-//	File tfcore.File
-//}
