f88a031499083031ad3e95a3e63aad986fe2a340
Matthew James Briggs <matthew.james.briggs@gmail.com>
2019-02-23 15:36:36 -0800

    cleanup

--- cmd/tftpd/main.go
+++ cmd/tftpd/main.go
@@ -20,6 +20,8 @@ func main() {
 		flog.Error(err.Error())
 		os.Exit(1)
 	}
+
+	flog.InfoAlways("successful exit")
 }
 
 func run() error {
@@ -30,31 +32,31 @@ func run() error {
 	srvWait.Add(1)
 	var srvErr error
 
+	// Serve blocks until Stop is called, so we run it on its own goroutine
+	// This function will exit once srvr.Stop is called
 	go func() {
 		flog.InfoAlways("tftp server is starting on port %d", 69)
 		srvErr = srvr.Serve(69)
 		srvWait.Done()
 	}()
 
-	//err := srvr.Serve(69)
-	//
-	//if err != nil {
-	//	flog.Error(err.Error())
-	//	os.Exit(1)
-	//}
-
+	// listen for sigint (i.e. control-c) to stop the server
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt)
 	sig := <-sigChan
 	if sig == syscall.SIGINT {
 		fmt.Print("\n")
-		flog.InfoAlways("SIGINT")
+		flog.InfoAlways("SIGINT received - stopping tftp server")
 		err := srvr.Stop()
 		if err != nil {
+			// TODO - we lose the Serve function's error, if any, in this case
 			return err
 		}
 	}
 
+	// wait for the Serve goroutine to stop
 	srvWait.Wait()
+
+	// return error if one was received from the Serve function
 	return srvErr
 }
